00cc0601245f6b7d760f1ab3b6865cc012a9eb85
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-05-30 22:06:52 -0300

    trying to get the number of states

--- Module_02/BrazilianStatesCities/main.js
+++ Module_02/BrazilianStatesCities/main.js
@@ -2,21 +2,35 @@ const fs = require('fs')
 
 
 
-function readAplicationFiles(){
+function readAplicationFiles() {
     fs.readFile('Module_02/BrazilianStatesCities/Original_Files/Estados.json', (err, data) => {
         if (err) throw err;
 
-        let  exemplo = JSON.parse(data.toString());
-        console.log(exemplo);
+        let states = JSON.parse(data.toString());
+        console.log(states.length); 
         
+
         fs.readFile('Module_02/BrazilianStatesCities/Original_Files/Cidades.json', (err, data) => {
             if (err) throw err;
 
-            let exemplo2 = JSON.parse(data.toString());
-
+            let cities = JSON.parse(data.toString());
+            
+            // states.forEach((state) => createStateFile(state, cities))
         });
     });
 
 }
+
+
+function createStateFile(state, cities) {
+    let stateCities = cities.filter(city => { city.Estado === state.ID });
+
+    fs.writeFile(`Module_02/BrazilianStatesCities/EachStateFiles/${state.Sigla}.json`,
+        JSON.stringify(stateCities, null, 4), (err) => {
+            if (err) throw err;
+            console.log('Deu certo');
+        });
+}
+
 
 readAplicationFiles();
