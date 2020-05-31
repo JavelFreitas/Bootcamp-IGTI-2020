0616e11c1caed25128b659e45628bd82707a86f0
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-05-30 23:33:03 -0300

    Counting cities for inserted state

--- Module_02/BrazilianStatesCities/main.js
+++ Module_02/BrazilianStatesCities/main.js
@@ -1,4 +1,6 @@
-const fs = require('fs')
+const fs = require('fs');
+const express = require('express');
+const app = express();
 
 
 
@@ -7,15 +9,15 @@ function readAplicationFiles() {
         if (err) throw err;
 
         let states = JSON.parse(data.toString());
-        console.log(states.length); 
-        
+        // console.log(states.length);
+
 
         fs.readFile('Module_02/BrazilianStatesCities/Original_Files/Cidades.json', (err, data) => {
             if (err) throw err;
 
             let cities = JSON.parse(data.toString());
-            
-            // states.forEach((state) => createStateFile(state, cities))
+
+            states.forEach((state) => createStateFile(state, cities));
         });
     });
 
@@ -23,7 +25,7 @@ function readAplicationFiles() {
 
 
 function createStateFile(state, cities) {
-    let stateCities = cities.filter(city => { city.Estado === state.ID });
+    const stateCities = cities.filter(city => city.Estado === state.ID);
 
     fs.writeFile(`Module_02/BrazilianStatesCities/EachStateFiles/${state.Sigla}.json`,
         JSON.stringify(stateCities, null, 4), (err) => {
@@ -32,5 +34,49 @@ function createStateFile(state, cities) {
         });
 }
 
+async function countStates() {
+    let data = fs.readFileSync('Module_02/BrazilianStatesCities/Original_Files/Estados.json');
 
-readAplicationFiles();
+    let states = JSON.parse(data.toString());
+
+    return states.length;
+}
+
+async function countCities(state){
+    
+    let data = fs.readFileSync(`Module_02/BrazilianStatesCities/EachStateFiles/${state}.json`);
+
+    let cities = JSON.parse(data.toString());
+
+    return cities.length;  
+}
+
+// console.log(countStates());
+// console.log(countCities('CE'));
+
+
+
+
+app.get('/buildFiles', (_, res) => {
+    readAplicationFiles();
+    res.end();
+});
+
+app.get('/totalOfStates', (_, res) => {
+    countStates().then(
+        (numberOfStates) => res.send(`${numberOfStates}`) 
+    );
+});
+
+app.get('/numberOfCities/:state', (req, res) => {
+    countCities(req.params.state).then(
+        (totalCities) => res.send(`${totalCities}`)
+    ); 
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log(`App listening onport 3000`);
+}); 
