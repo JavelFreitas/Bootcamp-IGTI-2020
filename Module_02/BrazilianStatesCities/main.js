a51dc533072bc3fb750fd86c98ec2475dd943b7c
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-05-30 21:41:30 -0300

    trying to create all states files

--- Module_02/BrazilianStatesCities/main.js
+++ Module_02/BrazilianStatesCities/main.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+const fs = require('fs')
+
+
+
+function readAplicationFiles(){
+    fs.readFile('Module_02/BrazilianStatesCities/Original_Files/Estados.json', (err, data) => {
+        if (err) throw err;
+
+        let  exemplo = JSON.parse(data.toString());
+        console.log(exemplo);
+        
+        fs.readFile('Module_02/BrazilianStatesCities/Original_Files/Cidades.json', (err, data) => {
+            if (err) throw err;
+
+            let exemplo2 = JSON.parse(data.toString());
+
+        });
+    });
+
+}
+
+readAplicationFiles();
